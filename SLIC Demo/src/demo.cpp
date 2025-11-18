401afee21e886853d0318b331085691dbe3aa226
Chandler Calkins <chandlerjaycalkins@gmail.com>
2025-11-18 15:19:14 -0800

    Added code to count the number of pixels in each superpixel in slic demo

--- SLIC Demo/src/demo.cpp
+++ SLIC Demo/src/demo.cpp
@@ -34,10 +34,30 @@ int main(int argc, char* argv[])
 
 	const int avg_superpixel_size = 100; // Default: 100
 	const float smoothness = 100.0f; // Default: 10.0
-	const int min_superpixel_size_percent = 5;
+	const int min_superpixel_size_percent = 4;
 	Ptr<ximgproc::SuperpixelSLIC> slic = ximgproc::createSuperpixelSLIC(input_image, ximgproc::SLIC, avg_superpixel_size, smoothness);
 	slic->iterate();
 	slic->enforceLabelConnectivity(min_superpixel_size_percent);
+
+	// Gets 2D array of the superpixel each pixel is a part of
+	Mat labels;
+	slic->getLabels(labels);
+	int superpixel_count = slic->getNumberOfSuperpixels();
+
+	// Counts how many pixels are in each superpixel
+	unsigned long* pixel_count = new unsigned long[superpixel_count];
+	for (int row = 0; row < labels.rows; row += 1)
+	{
+		for (int col = 0; col < labels.cols; col += 1)
+		{
+			pixel_count[labels.at<int>(row, col)] += 1;
+		}
+	}
+	// Prints out the pixel count of each superpixel
+	for (int i = 0; i < superpixel_count; i += 1)
+	{
+		std::cout << i << ": " << pixel_count[i] << std::endl;
+	}
 
 	// Gets overlay image of superpixels
 	Mat superpixels;
