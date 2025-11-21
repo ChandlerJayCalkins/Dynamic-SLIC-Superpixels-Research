37a69ef77f0fa60712821b477ae0823c937a8487
LukeErdy <112845457+LukeErdy@users.noreply.github.com>
2025-11-20 17:00:11 -0800

    Add files via upload

--- SLIC Demo/src/SLICHashTable.hpp
+++ SLIC Demo/src/SLICHashTable.hpp
@@ -3,20 +3,30 @@
 
 #include <opencv2/core/mat.hpp>
 
+/*** a HashKey pointer will point to an array of size superpixel_count (+1 if labels start at 1), 
+in which HashKeys will occupy the indices of the superpixels they represent.
+Each pixel will be looped through and the respective HashKey structs of the superpixels they belong
+to will be dynamically updated.  ***/
+typedef struct {
+    int l_avg;
+    int a_avg;
+    int b_avg;
+    std::pair<int, int> x_range;
+    std::pair<int, int> y_range;
+    cv::Mat *original_image;
+} HashKey;
+
 class SLICHashTable {
     public:
         // called for hashing segmented images, and storing them in the instance of this class
-        void Hash(const cv::Mat& input_image, const cv::Mat& labels) {
-            // deep copies
-            input_image_cpy(input_image.clone());
-            labels_cpy(labels.clone());
+        void Hash(const cv::Mat& input_image,
+                  const cv::Mat& labels,
+                  int superpixel_count)
+        {
+            
         }
 
     private:
-        // copies of the Mats, owned by the class
-        cv::Mat input_image_cpy;
-        cv::Mat labels_cpy;
-
         // hash table structure
 };
 
