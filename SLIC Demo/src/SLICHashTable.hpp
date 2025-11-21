08be8ad4533debc7ec0beafa0fadd76efea288c6
LukeErdy <112845457+LukeErdy@users.noreply.github.com>
2025-11-20 18:48:25 -0800

    Add files via upload

--- SLIC Demo/src/SLICHashTable.hpp
+++ SLIC Demo/src/SLICHashTable.hpp
@@ -2,6 +2,7 @@
 #define SLICHASHTABLE_HPP
 
 #include <opencv2/core/mat.hpp>
+#include <stdlib.h>
 
 /*** a HashKey pointer will point to an array of size superpixel_count (+1 if labels start at 1), 
 in which HashKeys will occupy the indices of the superpixels they represent.
@@ -19,11 +20,26 @@ typedef struct {
 class SLICHashTable {
     public:
         // called for hashing segmented images, and storing them in the instance of this class
+        // expects a cielab image for input_image
         void Hash(const cv::Mat& input_image,
                   const cv::Mat& labels,
                   int superpixel_count)
         {
-            
+            HashKey *superpixels = (HashKey*) calloc(superpixel_count, sizeof(HashKey));
+            for (int row = 0; row < labels.rows; row++) {
+                for (int col = 0; col < labels.cols; col++) {
+                    cv::Vec3b lab_pixel = input_image.at<cv::Vec3b>(row, col);
+                    int sp = labels.at<int>(row, col);
+                    HashKey curr = superpixels[sp];
+                    curr.l_avg += lab_pixel[0];
+                    curr.a_avg += lab_pixel[1];
+                    curr.b_avg += lab_pixel[2];
+                    if (curr.x_range.first > col) curr.x_range.first = col;
+                    if (curr.x_range.second < col) curr.x_range.second = col;
+                    if (curr.y_range.first > row) curr.y_range.first = row;
+                    if (curr.y_range.second < row) curr.y_range.second = row;
+                }
+            }
         }
 
     private:
